ec0039947cc59c8934be0a125c7e26caed7021e3
Sam McGowan <the.sam.mcg@protonmail.com>
2024-01-21 18:51:14 +0000

    Extract module.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use cli::{Cli, Command};
 
 mod cli;
 
+#[cfg(feature = "test_suite")]
+mod test_suite;
+
 #[derive(thiserror::Error, Debug)]
 enum CompilerError {
     #[error(transparent)]
@@ -28,125 +31,4 @@ fn main() -> CompilerResult<()> {
     }
 
     Ok(())
-}
-
-#[cfg(feature = "test_suite")]
-mod test_suite {
-    use std::fs::{self, DirEntry};
-    use std::path::PathBuf;
-
-    use owo_colors::OwoColorize;
-
-    use crate::CompilerResult;
-
-    fn get_stage_path(stage: usize) -> PathBuf {
-        let mut path = PathBuf::new();
-
-        path.push("test_suite");
-        path.push(format!("stage_{stage}"));
-
-        path
-    }
-
-    pub fn run_test_suite(stage: usize) -> CompilerResult<()> {
-        println!("{}", "running tests...".blue().bold());
-
-        let mut output = TestOutput::new(0, 0);
-
-        for stage in 1..=stage {
-            let path = get_stage_path(stage);
-
-            let path_valid = path.join("valid");
-            let path_invalid = path.join("invalid");
-
-            for entry in fs::read_dir(path_valid)? {
-                output.append(run_tests_in_entry(entry?, false)?);
-            }
-
-            for entry in fs::read_dir(path_invalid)? {
-                output.append(run_tests_in_entry(entry?, true)?);
-            }
-        }
-
-        print!("\n{} of {} tests passed - ", output.passed, output.total);
-
-        if output.passed == output.total {
-            println!("{}", "all ok".green());
-        } else {
-            println!(
-                "{}",
-                format_args!("{} tests failed", output.total - output.passed)
-                    .red()
-                    .bold()
-            );
-        }
-
-        Ok(())
-    }
-
-    fn run_tests_in_entry(entry: DirEntry, expect_error: bool) -> CompilerResult<TestOutput> {
-        let metadata = entry.metadata()?;
-        let path = entry.path();
-
-        if metadata.is_dir() {
-            let mut output = TestOutput::new(0, 0);
-
-            for entry in fs::read_dir(path)? {
-                let entry = entry?;
-                output.append(run_tests_in_entry(entry, expect_error)?);
-            }
-
-            Ok(output)
-        } else {
-            let s = fs::read_to_string(&path)?;
-
-            print!(" - {} - ", path.display());
-            let result = run_source(s);
-
-            let passed = if expect_error {
-                if result.is_err() {
-                    println!("{}", "ok".green().bold());
-                    true
-                } else {
-                    println!("{}", "err (unexpectedly compiled)".red().bold());
-                    false
-                }
-            } else {
-                match result {
-                    Ok(_) => {
-                        println!("{}", "ok".green().bold());
-                        true
-                    }
-                    Err(err) => {
-                        println!("{}", "err (failed to compile)".red().bold());
-                        println!("  {err}");
-                        false
-                    }
-                }
-            };
-
-            Ok(TestOutput::new(if passed { 1 } else { 0 }, 1))
-        }
-    }
-
-    #[derive(Clone, Copy)]
-    struct TestOutput {
-        passed: usize,
-        total: usize,
-    }
-
-    impl TestOutput {
-        fn new(passed: usize, total: usize) -> Self {
-            Self { passed, total }
-        }
-
-        fn append(&mut self, other: Self) {
-            self.passed += other.passed;
-            self.total += other.total;
-        }
-    }
-
-    fn run_source(_s: String) -> CompilerResult<()> {
-        Ok(())
-    }
 }
