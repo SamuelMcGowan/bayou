4e02dabd0b7e70858afb02ee9ba17fc704e6f631
Sam McGowan <the.sam.mcg@protonmail.com>
2024-04-04 18:14:59 +0100

    Fix tests.

--- crates/bayou_compiler/src/parser/tests.rs
+++ crates/bayou_compiler/src/parser/tests.rs
@@ -3,14 +3,17 @@ use target_lexicon::Triple;
 use crate::compiler::{PackageCompilation, Session};
 
 fn test_compiles(source: &str, should_compile: bool) {
-    let mut sess = Session::new(vec![], Triple::host()).expect("failed to create session");
+    let mut sess = Session::new(vec![], Triple::host());
 
     let compiled = PackageCompilation::parse(&mut sess, "tests", source)
         .and_then(|pkg| pkg.compile(&mut sess))
         .is_ok();
 
     match (compiled, should_compile) {
-        (false, true) => panic!("failed to compile: {source:?}"),
+        (false, true) => panic!(
+            "failed to compile: {source:?}, diagnostics: {:?}",
+            sess.diagnostics
+        ),
         (true, false) => panic!("unexpectedly compiled: {source:?}"),
         _ => {}
     }
@@ -18,60 +21,60 @@ fn test_compiles(source: &str, should_compile: bool) {
 
 #[test]
 fn multi_digit() {
-    test_compiles("int main() { return 100; }", true);
+    test_compiles("func main() -> int { return 100; }", true);
 }
 
 #[test]
 fn newlines() {
-    test_compiles("\nint\nmain\n(\n)\n{\nreturn\n0\n;\n}", true);
+    test_compiles("\nfunc\nmain\n(\n)\n->\nint\n{\nreturn\n0\n;\n}", true);
 }
 
 #[test]
 fn no_newlines() {
-    test_compiles("int main(){return 0;}", true);
+    test_compiles("func main()->int{return 0;}", true);
 }
 
 #[test]
 fn spaces() {
-    test_compiles("   int   main    (  )  {   return  0 ; }", true);
+    test_compiles("  func  main  (  )  ->  int  {  return  0  ;  }", true);
 }
 
 #[test]
 fn return_0() {
-    test_compiles("int main() { return 0; }", true);
+    test_compiles("func main() -> int { return 0; }", true);
 }
 
 #[test]
 fn return_2() {
-    test_compiles("int main() { return 2; }", true);
+    test_compiles("func main() -> int { return 2; }", true);
 }
 
 #[test]
 fn missing_paren() {
-    test_compiles("int main( { return 0; }", false);
+    test_compiles("func main( { return 0; }", false);
 }
 
 #[test]
 fn missing_retval() {
-    test_compiles("int main() { return; }", false);
+    test_compiles("func main() { return; }", false);
 }
 
 #[test]
 fn no_brace() {
-    test_compiles("int main() { return 0;", false);
+    test_compiles("func main() -> int { return 0;", false);
 }
 
 #[test]
 fn no_semicolon() {
-    test_compiles("int main() { return 0 }", false);
+    test_compiles("func main() -> int { return 0 }", false);
 }
 
 #[test]
 fn no_space() {
-    test_compiles("int main() { return0; }", false);
+    test_compiles("func main() -> int { return0; }", false);
 }
 
 #[test]
 fn wrong_case() {
-    test_compiles("int main() { RETURN 0; }", false);
+    test_compiles("func main() -> int { RETURN 0; }", false);
 }
