447f884c109ef0e885d7cc24ce5977023d0d3170
Sam McGowan <the.sam.mcg@protonmail.com>
2024-04-08 21:16:03 +0100

    Dependency stuff.

--- crates/bayou_backend/src/linker.rs
+++ crates/bayou_backend/src/linker.rs
@@ -1,7 +1,7 @@
 use std::ffi::OsStr;
 use std::process::Command;
 
-use target_lexicon::{Environment, Triple};
+use bayou_session::target_lexicon::{Environment, Triple};
 
 #[derive(thiserror::Error, Debug)]
 pub enum LinkerError {
