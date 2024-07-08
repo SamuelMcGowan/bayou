ca84981019ba2c571c4641f876c190da9261865b
Sam McGowan <the.sam.mcg@protonmail.com>
2024-07-08 11:16:08 +0100

    Add snippet to entrypoint error.

--- crates/bayou_middle/src/entry_point.rs
+++ crates/bayou_middle/src/entry_point.rs
@@ -2,20 +2,36 @@ use bayou_interner::Interner;
 use bayou_ir::symbols::{GlobalId, Symbols};
 use bayou_ir::Type;
 use bayou_session::diagnostics::prelude::*;
+use bayou_session::sourcemap::SourceSpan;
 
-// TODO: store spans
 pub enum EntrypointError {
     Missing,
-    WrongSignature { expected: Type, found: Type },
+    WrongSignature {
+        expected: Type,
+
+        found: Type,
+        found_span: SourceSpan,
+    },
 }
 
 impl IntoDiagnostic<()> for EntrypointError {
     fn into_diagnostic(self, _cx: ()) -> Diagnostic {
         match self {
             EntrypointError::Missing => Diagnostic::error().with_message("`main` function missing"),
-            EntrypointError::WrongSignature { expected, found } => Diagnostic::error()
+
+            EntrypointError::WrongSignature {
+                expected,
+                found,
+                found_span,
+            } => Diagnostic::error()
                 .with_message(format!(
-                    "expected main function with return type {expected:?}, but it returned type {found:?}"
+                    "expected main function with return type {expected:?}, \
+                    but it returned type {found:?}"
+                ))
+                .with_snippet(Snippet::primary(
+                    "main function has wrong signature",
+                    found_span.source_id,
+                    found_span.span,
                 )),
         }
     }
@@ -36,7 +52,9 @@ pub fn check_entrypoint(symbols: &Symbols, interner: &Interner) -> Result<(), En
     if func.ret_ty != Type::I64 {
         return Err(EntrypointError::WrongSignature {
             expected: Type::I64,
+
             found: func.ret_ty,
+            found_span: func.ret_ty_span,
         });
     }
 
